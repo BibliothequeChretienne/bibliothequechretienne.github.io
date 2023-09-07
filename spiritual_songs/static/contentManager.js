127ce18170dd7fb564d071fe472da3a369b378ca
Germain <adedihagermain@gmail.com>
2023-09-08 02:17:24 +0300

    spiritual_song_s files added

---
+++ spiritual_songs/static/contentManager.js
@@ -0,0 +1,193 @@
+
+// const notesButton = document.getElementById("notes");
+// const icon = notesButton.querySelector("i");
+// let noteIsSlashed = false;
+
+// notesButton.addEventListener("click", () => {
+//     if (noteIsSlashed) {
+//         icon.classList.remove("fa-music-slash"); // Remove the class to unslash
+//         icon.classList.add("fa-music"); // Add the class for non-slashed
+//     } else {
+//         icon.classList.remove("fa-music"); // Remove the class for non-slashed
+//         icon.classList.add("fa-music-slash"); // Add the class to slash
+//     }
+//     noteIsSlashed = !noteIsSlashed; // Toggle the state
+// });
+
+
+
+// const textsButton = document.getElementById("texts");
+// const textSpan = document.getElementById("textSpan");
+// let textIsSlashed = false;
+
+// textsButton.addEventListener("click", () => {
+//   if (textIsSlashed) {
+//     textSpan.textContent = "T";
+//   } else {
+//     textSpan.innerHTML = '<i class="fa-solid fa-text-slash"></i>';
+//   }
+//   textIsSlashed = !textIsSlashed;
+// });
+
+
+//----------------------------------------------- note-button----------------------------------------------//
+
+
+const notesDiv = document.getElementById("notesDiv");;
+const textsDiv = document.getElementById("textsDiv")
+
+
+const notesButton = document.getElementById("notes");
+const notesIcon = notesButton.querySelector("i");
+let noteIsSlashed = false;
+
+notesButton.addEventListener("click", () => {
+  if (noteIsSlashed) {
+    notesIcon.classList.remove("fa-music-slash");
+    notesIcon.classList.add("fa-music");
+  } else {
+    notesIcon.classList.remove("fa-music");
+    notesIcon.classList.add("fa-music-slash");
+
+    // Unslash the text button
+    textSpan.textContent = "T";
+    textIsSlashed = false;
+  }
+  noteIsSlashed = !noteIsSlashed;
+  updateDivVisibility();
+});
+
+
+//-----------------------------------------------text-button----------------------------------------------//
+const textsButton = document.getElementById("texts");
+const textSpan = document.getElementById("textSpan");
+let textIsSlashed = false;
+
+textsButton.addEventListener("click", () => {
+  if (textIsSlashed) {
+    textSpan.textContent = "T";
+  } else {
+    textSpan.innerHTML = '<i class="fa-solid fa-text-slash"></i>';
+
+    // Unslash the notes button
+    notesIcon.classList.remove("fa-music-slash");
+    notesIcon.classList.add("fa-music");
+    noteIsSlashed = false;
+  }
+  textIsSlashed = !textIsSlashed;
+  updateDivVisibility();
+});
+
+
+// update Div visibility 
+function updateDivVisibility(){
+  // if (noteIsSlashed && textIsSlashed){
+  //    // If both buttons are slashed, show notes only,( hide both divs )
+  //   notesDiv.style.display ="block";
+  //   textsDiv.style.display="none"
+    
+  // }
+  // else 
+  if(noteIsSlashed){ // If notes button is slashed, hide notesDiv, show textsDiv
+    notesDiv.style.display ="none";
+    textsDiv.style.display = "block";
+
+  }
+  else if(textIsSlashed){  // If text button is slashed, hide textsDiv, show notesDiv
+    notesDiv.style.display = "block";
+    textsDiv.style.display = "none";
+  }
+  else{
+    // If neither button is slashed, show both divs
+    notesDiv.style.display = "block";
+    textsDiv.style.display = "block";
+  }
+
+}
+
+
+// refresh the songs.html page
+        const button1 = document.getElementById('voice1');
+        const button2 = document.getElementById('voice2');
+        const button3 = document.getElementById('voice3');
+        const button4 = document.getElementById('voice4');
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  const textContent = localStorage.getItem('textContent');
+  const svgFilePath = localStorage.getItem('svgFilePath');
+  const audioPlayer = document.getElementById('audio-player');
+  const savedMP3URL = localStorage.getItem('savedMP3');
+
+
+if (textContent) {
+      document.getElementById('textsDiv').innerHTML = `<pre>` + textContent + `</pre>`;
+  }
+
+  if (svgFilePath) {
+    document.getElementById('notesDiv').innerHTML = `<img src="` + svgFilePath + `" alt="song_s notes" ">`;
+}
+
+  if (savedMP3URL) {
+    // Set the src attribute of the audio element to the saved MP3 file URL.
+    audioPlayer.src = savedMP3URL + `1.mp3`;
+
+    button1.addEventListener('click' ,function(){
+      audioPlayer.src = savedMP3URL + `1.mp3`;
+    });
+    button2.addEventListener('click' ,function(){
+      audioPlayer.src = savedMP3URL + `2.mp3`;
+    });
+    button3.addEventListener('click' ,function(){
+      audioPlayer.src = savedMP3URL + `3.mp3`;
+    });
+    button4.addEventListener('click' ,function(){
+      audioPlayer.src = savedMP3URL + `4.mp3`;
+    });
+
+  } else {
+    alert('No saved MP3 found.');
+  }
+
+  // if(mp3FilePath){
+  //   const audioPlayer = document.getElementById('audio-player');
+  //   const playButton = document.getElementById('play-button');
+  //   playButton.addEventListener('click', function () {
+  //     audioPlayer.src=mp3FilePath;
+  //     audioPlayer.play();
+
+  //   });
+
+  // }
+});
+
+// -------------------------------repeating button for the audio file ----------------------------------//
+
+const repeatButton = document.getElementById('repeatButton');
+let isRepeating = false;
+
+// function repeatAudio() {
+//   setTimeout(function() {
+//       audioPlayer.currentTime = 0; // Rewind to the beginning
+//       audioPlayer.play(); // Play again after 2 seconds
+//   }, 2000); // 2000 milliseconds = 2 seconds
+// }
+
+// audioPlayer.addEventListener('ended', function(){
+//   if (isRepeating){
+//     repeatAudio();
+//   }
+// });
+
+repeatButton.addEventListener('click', function(){
+  audioPlayer.loop="loop";
+  // if(isRepeating){
+  //   repeatButton.style.backgroundColor='green';
+  //   audioPlayer.loop=`loop`;
+  // }
+  // else{
+  //   repeatButton.style.backgroundColor='white';
+  // }
+});
+
+
---
+++ spiritual_songs/static/search.js
@@ -0,0 +1,97 @@
+const analyzeButton = document.getElementById('analyze-button');
+const searchInput = document.querySelector('.search-input');
+const inputField = document.getElementById("inputField")
+
+
+analyzeButton.addEventListener('click', function() {
+    analyseInput();
+});
+
+inputField.addEventListener('keydown', function(event){
+    if (event.keyCode === 13){
+        analyzeButton.click();
+    }
+}); 
+
+
+
+// ------------------FONCTION D"ANALYSE DES ENTREES-----------------------//
+function analyseInput(){
+    const searchText = searchInput.value.trim();
+
+    const search_results = document.getElementById("search_results");
+
+    if (searchText === '') {  // Vibrate the input field
+        searchInput.classList.add('vibrate');
+        setTimeout(() => {
+            searchInput.classList.remove('vibrate');
+        }, 10000);
+    } else {
+        if (!isNaN(searchText)) {
+                                localStorage.setItem('svgFilePath', '');
+                                localStorage.setItem('savedMP3',  '');//URL.createObjectURL(blob));
+                                localStorage.setItem('textContent','' );
+
+
+            const numero = parseInt(searchText)
+            const textFilePath = `/song_texts/${numero}.txt`; // looking for the corresponding text file
+            const svgFilePath =`/song_notes/${numero}.svg`; // looking for the corresponding note svg file
+            const mp3FilePath = `/song_melodies/${numero}_`; // looking for the corresponding mp3 file. voice 1 by default
+
+///////////////////////////////////////////// A REVOIR LES NOTES DE MUSIQUES //////////////////////////
+            fetch(svgFilePath)
+                .then(response => response.blob())
+                .then( svgContent=> {
+                    // localStorage.setItem('svgContent', svgContent);
+                    localStorage.setItem('svgFilePath', svgFilePath);
+
+                })
+                ///// FETCHING THE MP3 FILE///
+
+            fetch(mp3FilePath)
+                .then(response => response.blob())
+                .then(blob => {
+                    localStorage.setItem('savedMP3', mp3FilePath );//URL.createObjectURL(blob));
+
+                })
+            fetch(textFilePath)
+                .then(response => response.text())
+                .then(textContent =>{
+                            localStorage.setItem('textContent', textContent);
+                        window.location.href = 'index.html'
+                        // search_results.textContent = "Number" + numero +"\n" + textContent;               
+                })
+                .catch(error => {
+                    search_results.textContent = "Ощибка загрузки файла. Возможно файл не найден!"
+                    alert(error(error));
+                });
+        } else {
+            // --------------------------------TEXT RESEARCH ACTIONS--------------------------//
+            alert('Text action:' + searchText);
+        }
+    }
+}
+
+
+
+//-----------------------------------affichage des notes------------------------------//
+
+// document.getElementById('searchButton').addEventListener('click', function() {
+//     const inputNumber = document.getElementById('inputNumber').value;
+//     const svgContainer = document.getElementById('svgContainer');
+//     svgContainer.innerHTML = ''; // Clear previous content
+
+//     const svgFilePath = `path/to/files/${inputNumber}.svg`; // Adjust the path to your files
+
+//     fetch(svgFilePath)
+//         .then(response => response.text())
+//         .then(svgContent => {
+//             const svgElement = document.createElement('div');
+//             svgElement.innerHTML = svgContent;
+//             svgContainer.appendChild(svgElement);
+//         })
+//         .catch(error => {
+//             console.error(error);
+//             svgContainer.textContent = 'SVG not found for the input number.';
+//         });
+// });

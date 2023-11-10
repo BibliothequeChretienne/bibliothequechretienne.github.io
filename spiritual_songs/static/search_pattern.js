422f69cbd1b316571c8e06ae395ea9970833c5c2
Germain <adedihagermain@gmail.com>
2023-11-10 22:17:32 +0300

    comment deleted

--- spiritual_songs/static/search_pattern.js
+++ spiritual_songs/static/search_pattern.js
@@ -1,33 +1,3 @@
-// const fs = require('fs');
-
-// // Function to search for matches in a text file
-// function searchMatchesInFile(inputText, filePath) {
-//   const fileContent = fs.readFileSync(filePath, 'utf8');
-//   const lines = fileContent.split('\n');
-//   const matchedLineNumbers = [];
-
-//   for (let i = 0; i < lines.length; i++) {
-//     if (lines[i].includes(inputText)) {
-//       // If the line contains the input text, add its line number to the result
-//       matchedLineNumbers.push(i + 1); // Adding 1 because line numbers start from 1
-//     }
-//   }
-
-//   return matchedLineNumbers;
-// }
-
-// // Example usage:
-// const inputText = 'search me';
-// const filePath = 'song_lyrics.txt'; // Replace with the path to your text file
-
-// const matchedLineNumbers = searchMatchesInFile(inputText, filePath);
-
-// if (matchedLineNumbers.length > 0) {
-//   console.log(`Matches found in lines: ${matchedLineNumbers.join(', ')}`);
-// } else {
-//   console.log('No matches found.');
-// }
-
 const filePath = '../song_texts/songs.txt';
 
 function searchText() {
